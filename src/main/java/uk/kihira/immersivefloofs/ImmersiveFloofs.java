8cbe6385fab415ce99b4fb677d6a26b2726961ef
Kihira <kihira@kihirakreations.co.uk>
2016-08-31 15:15:01 +0100

    Initial setup

---
+++ src/main/java/uk/kihira/immersivefloofs/ImmersiveFloofs.java
@@ -0,0 +1,7 @@
+package uk.kihira.immersivefloofs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "immersivefloofs", name = "Immersive Floofs", version = "1.0.0")
+public class ImmersiveFloofs {
+}
